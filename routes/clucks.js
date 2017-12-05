f1ae96735062a0417521c5bbbecadb4b2e662f91
chilidoggca <jack.chili.lee@gmail.com>
2017-12-05 12:04:52 -0800

    added 1)form page for users to submit content and upload pictures and 2)index page display posts

--- routes/clucks.js
+++ routes/clucks.js
@@ -7,57 +7,42 @@ const multer = require('multer');
 const UPLOADS_DIR = 'uploads';
 const upload = multer({dest: path.join(__dirname, '..', 'public', UPLOADS_DIR)});
 
-// PATH: /posts/new VERB: GET Serves form for creating posts
 router.get('/new', (req, res) => {
-  res.render('posts/new');
+  res.render('clucks/new');
 });
 
-// PATH: /posts/idName VERB: Get Display post with idName
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  knex
-    .first()
-    .from('posts')
-    .where({id})
-    .then(post => {
-      res.render('posts/show', {post});
-    })
-    .catch(err => res.send(err));
-});
-
-// PATH: /posts VERB: POST Creating new posts
 router.post('/', upload.single('picture'), (req, res) => {
-  const username = req.body.username;
-  const content = req.body.content;
-  // console.log('file', req.file);
+  const username = req.body.username || 'anonymous';
+  const content = req.body.content || '';
 
-  // this if checks that each value exists. If any of the is undefined or something that
-  // is not truthy, it will go to the else block.
-  if (username && content && req.file) {
+  if (req.file) {
     const filename = req.file.filename;
-    const picture_path = path.join(UPLOADS_DIR, filename);
-
+    const image_url = `/${path.join(UPLOADS_DIR, filename)}`;
     knex
-      .insert({username: username, content: content, picture_path: picture_path})
-      .into('posts')
-      .returning('id')
-      .then(result => res.redirect(`/posts/${result}`))
+      .insert({username, content, image_url})
+      .into('clucks')
+      // .returning('id')
+      .then(result => res.redirect(`/clucks`))
       .catch(err => res.send(err));
   } else {
-    res.locals.flash = ['Username, content and pictures are required!'];
-    res.render('posts/new');
+    image_url = '';
+    knex
+      .insert({username, content, image_url})
+      .into('clucks')
+      // .returning('id')
+      .then(result => res.redirect(`/clucks`))
+      .catch(err => res.send(err));
   }
-
 });
 
-// PATH: /posts VERB: GET List all the posts
 router.get('/', (req, res) => {
+  // res.render('./clucks/index');
   knex
     .select()
-    .from('posts')
+    .from('clucks')
     .orderBy('created_at', 'DESC')
-    .then(posts => {
-      res.render('posts/index', {posts: posts});
+    .then(clucks => {
+      res.render('clucks/index', {clucks});
     });
 });
 
