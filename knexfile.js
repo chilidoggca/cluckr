ad31e632a7e074cef6545eb6674f2a7e1ea0fc13
chilidoggca <jack.chili.lee@gmail.com>
2018-03-06 00:59:06 -0800

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
   production: {
     ...sharedConfig,
     connection: {
-      database: 'postgres://cbqvikukqioviv:b3f41cc461816d7b4d4e13cfbe7cd5f8c130dd61f1fadf4611289c873700d993@ec2-50-17-206-214.compute-1.amazonaws.com:5432/dd78qv9spj5j90'
+      database: process.env.HEROKU_POSTGRESQL_URL
     }
   }
 };
